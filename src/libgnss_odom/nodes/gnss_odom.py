be11efe3def31242f6e3921ee41b2da4246a3a93
k-alev <kos.alevizos@gmail.com>
2025-02-20 12:52:28 +0200

    misc filtering added

--- src/libgnss_odom/nodes/gnss_odom.py
+++ src/libgnss_odom/nodes/gnss_odom.py
@@ -36,9 +36,11 @@ class GNSSOdometry:
         
         self.use_odometry = rospy.get_param('~use_odometry', False)
         self.velocity_threshold = rospy.get_param('~velocity_threshold', 0.03)
-        self.distance_threshold = rospy.get_param('~distance_threshold', 0.5)
+        self.lower_distance_threshold = rospy.get_param('~lower_distance_threshold', 0.05)
+        self.upper_distance_threshold = rospy.get_param('~upper_distance_threshold', 0.2)
         self.initial_covariance = rospy.get_param('~initial_covariance', 0.1)
         self.heading_offset = rospy.get_param('~heading_offset',0.0)
+        self.heading_diff_publish_ths = rospy.get_param('~heading_diff_publish_ths',0.34)
         self.gnss_ellipsoid = rospy.get_param('~gnss_ellipsoid','WGS84')
         self.prev_fix = None
         self.prev_cmd = None
@@ -55,6 +57,10 @@ class GNSSOdometry:
         self.timeout = rospy.get_param('~timeout', self.num_fit_points*1.0/5.0 + 1) #num_points*topic_hz + 1
         
         self.fit_points = deque(maxlen=self.num_fit_points) 
+
+        # Publish cmd filtering
+        self.published_headings_length = rospy.get_param('~published_headings_length', 5)
+        self.published_headings = deque(maxlen=self.published_headings_length) 
         
         self.geod = Geod(ellps=self.gnss_ellipsoid)
 
@@ -131,51 +137,58 @@ class GNSSOdometry:
 
         return fitted_heading, covariance
 
-    def compute_odom_from_gnss(self, fix_data):
-        print("Fit point list len: {}".format(len(self.fit_points)))
-        if self.prev_fix is not None and self.prev_cmd is not None:
-            if self.prev_cmd.linear.x >= self.velocity_threshold: 
-                heading_, _, distance = self.geod.inv( self.prev_fix.longitude, self.prev_fix.latitude, fix_data.longitude, fix_data.latitude)
-                heading = None
-                if distance >= self.distance_threshold:
+    # def compute_odom_from_gnss(self, fix_data):
+    #     print("Fit point list len: {}".format(len(self.fit_points)))
+    #     if self.prev_fix is not None and self.prev_cmd is not None:
+    #         if self.prev_cmd.linear.x >= self.velocity_threshold: 
+    #             heading_, _, distance = self.geod.inv( self.prev_fix.longitude, self.prev_fix.latitude, fix_data.longitude, fix_data.latitude)
+    #             heading = None
+    #             if distance >= self.distance_threshold:
 
-                    if (self.use_fitted_heading):
-                        x, y, _ = self.geod.fwd(
-                        self.prev_fix.longitude, self.prev_fix.latitude,
-                        fix_data.longitude, fix_data.latitude
-                        )
-                        self.fit_points.append((x, y))
-                        if len(self.fit_points)>self.num_fit_points:
-                            self.fit_points.pop(0)
-                            heading, covariance = self.compute_fitted_heading()
-                            print("TAKIS")
-                    else:
-                        heading = math.radians(heading_)
-                        covariance = self.initial_covariance
+    #                 if (self.use_fitted_heading):
+    #                     x, y, _ = self.geod.fwd(
+    #                     self.prev_fix.longitude, self.prev_fix.latitude,
+    #                     fix_data.longitude, fix_data.latitude
+    #                     )
+    #                     self.fit_points.append((x, y))
+    #                     if len(self.fit_points)>self.num_fit_points:
+    #                         self.fit_points.pop(0)
+    #                         heading, covariance = self.compute_fitted_heading()
+    #                         print("TAKIS")
+    #                 else:
+    #                     heading = math.radians(heading_)
+    #                     covariance = self.initial_covariance
 
-                    print("PRINT")
-                    if (heading != None):
-                        print("Heading: {} | Cov: {}".format(heading,covariance))
-                        q = Quaternion.from_euler(0.0, 0.0, heading+self.heading_offset)
+    #                 print("PRINT")
+    #                 if (heading != None):
+    #                     print("Heading: {} | Cov: {}".format(heading,covariance))
+    #                     q = Quaternion.from_euler(0.0, 0.0, heading+self.heading_offset)
                         
-                        current_odom = Odometry()
-                        current_odom.header.stamp = fix_data.header.stamp
-                        current_odom.header.frame_id = fix_data.header.frame_id
-                        current_odom.pose.pose.orientation.x = q.x
-                        current_odom.pose.pose.orientation.y = q.y
-                        current_odom.pose.pose.orientation.z = q.z
-                        current_odom.pose.pose.orientation.w = q.w
-                        current_odom.pose.covariance[35] = covariance
+    #                     current_odom = Odometry()
+    #                     current_odom.header.stamp = fix_data.header.stamp
+    #                     current_odom.header.frame_id = fix_data.header.frame_id
+    #                     current_odom.pose.pose.orientation.x = q.x
+    #                     current_odom.pose.pose.orientation.y = q.y
+    #                     current_odom.pose.pose.orientation.z = q.z
+    #                     current_odom.pose.pose.orientation.w = q.w
+    #                     current_odom.pose.covariance[35] = covariance
                         
-                        self.odom_pub.publish(current_odom)
-                else:
-                    self.fit_points = []
+    #                     self.odom_pub.publish(current_odom)
+    #             else:
+    #                 self.fit_points = []
 
-        self.prev_fix = fix_data
+    #     self.prev_fix = fix_data
 
     def compute_odom_from_odometry(self, odom_data):
-        print("Fit point list len: {}".format(len(self.fit_points)))
+        # print("Fit point list len: {}".format(len(self.fit_points)))
+
+        # fit_points_yaw = []
+        # for pair in np.diff(self.fit_points, axis=0):
+        #     fit_points_yaw.append(math.atan2(pair[0],pair[1]))
+        # path_max_knee = max(abs(np.diff(fit_points_yaw)))
+
         if self.prev_odom is not None and self.prev_cmd is not None:
+            print("CHECKING =====")
             if abs(self.prev_cmd.linear.x) >= self.velocity_threshold:
                 # Compute distance moved
                 dx = odom_data.pose.pose.position.x - self.prev_odom.pose.pose.position.x
@@ -183,27 +196,46 @@ class GNSSOdometry:
                 distance = math.sqrt(dx**2 + dy**2)
                 heading = None
 
+                print("Distance {}".format(distance))
                 current_time = rospy.get_time()
-                if (distance >= self.distance_threshold): #or (current_time-self.last_published_time > self.timeout):
-                    if (self.use_fitted_heading):
-                        self.fit_points.append((odom_data.pose.pose.position.x, odom_data.pose.pose.position.y))
-                        if len(self.fit_points) >= self.num_fit_points:
-                            heading, covariance = self.compute_fitted_heading()
-                    else:
-                        heading = math.atan2(dy, dx)
-                        covariance = self.initial_covariance
-                    
-                    if heading!=None :
-                        print("Heading: {} | Cov: {}".format(heading,covariance))
-                        q = Quaternion.from_euler(0.0, 0.0, heading+self.heading_offset)
+                if (distance <= self.upper_distance_threshold):
+                    if (distance >= self.lower_distance_threshold): #or (current_time-self.last_published_time > self.timeout):
+                        if (self.use_fitted_heading):
+                            self.fit_points.append((odom_data.pose.pose.position.x, odom_data.pose.pose.position.y))
+                            print("Fit points {}".format(self.fit_points))
+                            if len(self.fit_points) >= self.num_fit_points:
+                                heading, covariance = self.compute_fitted_heading()
+                        else:
+                            heading = math.atan2(dy, dx)
+                            covariance = self.initial_covariance
                         
-                        odom_data.pose.pose.orientation.x = q.x
-                        odom_data.pose.pose.orientation.y = q.y
-                        odom_data.pose.pose.orientation.z = q.z
-                        odom_data.pose.pose.orientation.w = q.w
-                        # odom_data.pose.covariance[35] = self.initial_covariance
+                        print("Heading {}".format(heading))
+                        if heading!=None :
+                            # print("Heading: {} | Cov: {}".format(heading,covariance))
+                            q = Quaternion.from_euler(0.0, 0.0, heading+self.heading_offset)
+                            
+                            odom_data.pose.pose.orientation.x = q.x
+                            odom_data.pose.pose.orientation.y = q.y
+                            odom_data.pose.pose.orientation.z = q.z
+                            odom_data.pose.pose.orientation.w = q.w
+                            # odom_data.pose.covariance[35] = self.initial_covariance
+                            
+                            if len(self.published_headings)>=self.published_headings_length:
+                                # filtering out spikes
+                                if abs(np.mean(self.published_headings)-(heading+self.heading_offset))<self.heading_diff_publish_ths:
+                                    print("PUBLISHING")
+                                    self.odom_pub.publish(odom_data)
+                                else:
+                                    print("===================== NOT PUBLISHING ================================")
+                                    print("Heading List: {}".format(self.published_headings))
+                                    print("Current Heading: {}".format(heading+self.heading_offset))
+                            else:
+                                print("++++++++++++++++++++++++ SMALL LENGTH ++++++++++++++++++++++++++++++")
+                            self.published_headings.append(heading+self.heading_offset)
+
+
+                            # self.odom_pub.publish(odom_data)
                         
-                        self.odom_pub.publish(odom_data)
                 else:
                     #TODO: A time-related criterion should clear the deque as well
                     self.last_published_time = rospy.get_time()
